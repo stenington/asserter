bf6d6b5914f1910531009313255d1700dfad84d8
Mike Larsson <mikelarssonftw@gmail.com>
2013-07-16 17:15:46 -0400

    Make bin take args from env

--- bin/asserter.js
+++ bin/asserter.js
@@ -3,10 +3,12 @@
 const path = require('path');
 
 const PORT = process.env['PORT'] || 8888;
+const STATIC_DIR = process.env['STATIC_DIR'];
+const DATA_DIR = process.env['DATA_DIR'];
 
 var app = require('../').app.build({
-  dataDir: path.join(__dirname, '../test/data'),
-  staticDir: path.join(__dirname, '../test/static')
+  dataDir: path.join(__dirname, '..', STATIC_DIR),
+  staticDir: path.join(__dirname, '..', DATA_DIR)
 });
 app.listen(PORT, function() {
   console.log("Listening on port " + PORT + ".");
